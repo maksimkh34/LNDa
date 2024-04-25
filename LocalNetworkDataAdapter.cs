29ff28118b19a816856e6521eaf9d7b50d7fecf6
maksimkh34@dell <maksimkh34@duck.com>
2024-04-25 11:02:15 +0300

    lndadapter

---
+++ LocalNetworkDataAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LNDa
+{
+    internal class LocalNetworkDataAdapter
+    {
+        public delegate void DataRecived(string data);
+
+        public static void SendData(string ip, string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void StartPolling(DataRecived dataRecived)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
